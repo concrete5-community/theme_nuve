8c21bb0d384f5e67b1e2ea535ccce4f03ddaf3cc
Jacqmin Sébastien <seb@pc64.home>
2015-11-05 16:07:28 +0100

    First PRB

--- themes/nuve/view.php
+++ themes/nuve/view.php
@@ -2,13 +2,13 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 
 // -- Include different elements of the page -- \\
-$this->inc('elements/head.php'); 
-$this->inc('elements/header.php'); 
+$this->inc('elements/head.php');
+$this->inc('elements/header.php');
 ?>
 
-<main class="full main-container">
-	<?php print $innerContent; ?>		
+<main>
+	<?php print $innerContent; ?>
 </main>
 
 
-<?php $this->inc('elements/footer.php'); 
+<?php $this->inc('elements/footer.php');
