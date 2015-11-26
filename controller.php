92489f7fcf53ff30832144ae6d721199d433af86
Jacqmin Sébastien <seb@tellthem.be>
2015-11-26 15:18:59 +0100

    remove styles area from content.xml

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends \Concrete\Core\Package\Package {
 
 	protected $pkgHandle = 'theme_nuve';
 	protected $appVersionRequired = '5.7.5';
-	protected $pkgVersion = '0.2.6';
+	protected $pkgVersion = '0.2.8';
 	protected $pkgAllowsFullContentSwap = true;
 	protected $pkg;
 
--- themes/nuve/elements/multiple_area.php
+++ themes/nuve/elements/multiple_area.php
@@ -9,13 +9,14 @@ $number_of_areas = $number_of_areas === false ? 1 : $number_of_areas;
 if ($number_of_areas > 0 ) {
     for ($i=0; $i <= $number_of_areas; $i++) {
         $id = $i > 0 ?  $area_name . ' - ' . $i : $area_name;
+        $handle = strtolower($area_name) . '-' . $i;
         $area = new Area($id);
         $visible = $i === 0 ? 'visible' : '';
         $styleSet = $pageTheme->getAreaStyles($area,$c);
 
-        echo "<section class=\"cd-section $id $visible\">";
+        echo "<section class=\"cd-section $handle $visible\">";
         echo "<div class=\"cd-section-inner\">";
-        echo "<div class=\"cd-section-style\" style=\"$styleSet\">";
+        echo "<div class=\"cd-section-style cd-section-style-$handle\" style=\"$styleSet\">";
         echo "<div class=\"vertical-align\">";
         echo "<div>";
     	    $area->enableGridContainer();
